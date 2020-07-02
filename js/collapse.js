3cb960b96735b88ec666cde77b699b6895bbe930
rtelles64 <rtelles64@gmail.com>
2020-07-02 11:18:29 -0700

    Add JavaScript files with code

---
+++ js/collapse.js
@@ -0,0 +1,21 @@
+// collapsible script
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+function collapse () {
+    for (i = 0; i < coll.length; i++) {
+      coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight){
+          content.style.maxHeight = null;
+        } else {
+          content.style.maxHeight = content.scrollHeight + "px";
+        }
+      });
+    }
+};
+
+$(document).ready(
+    collapse()
+);
